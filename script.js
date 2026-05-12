3a8e7ee3a9356776814aaf0e1d02245c4490c07b
Jujutsu-Kaisen-123 <mofiyinfoluwaphillips@gmail.com>
2026-05-12 17:49:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPMmDG1D5meUlVJ0QnwEuYwnc-9Mt5bGw",
+    authDomain: "shopping-list-f4de7.firebaseapp.com",
+    databaseURL: "https://shopping-list-f4de7-default-rtdb.firebaseio.com",
+    projectId: "shopping-list-f4de7",
+    storageBucket: "shopping-list-f4de7.firebasestorage.app",
+    messagingSenderId: "405729694759",
+    appId: "1:405729694759:web:01770748a583900b282a02",
+    measurementId: "G-WGFKGPB1VL"
+  };
