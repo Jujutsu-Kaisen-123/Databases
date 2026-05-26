94742f6fc3d1b240858b4fa2372a7275f25aaf42
Jujutsu-Kaisen-123 <mofiyinfoluwaphillips@gmail.com>
2026-05-26 17:58:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBPMmDG1D5meUlVJ0QnwEuYwnc-9Mt5bGw",
-    authDomain: "shopping-list-f4de7.firebaseapp.com",
-    databaseURL: "https://shopping-list-f4de7-default-rtdb.firebaseio.com",
-    projectId: "shopping-list-f4de7",
-    storageBucket: "shopping-list-f4de7.firebasestorage.app",
-    messagingSenderId: "405729694759",
-    appId: "1:405729694759:web:01770748a583900b282a02",
-    measurementId: "G-WGFKGPB1VL"
+  authDomain: "shopping-list-f4de7.firebaseapp.com",
+  databaseURL: "https://shopping-list-f4de7-default-rtdb.firebaseio.com",
+  projectId: "shopping-list-f4de7",
+  storageBucket: "shopping-list-f4de7.firebasestorage.app",
+  messagingSenderId: "405729694759",
+  appId: "1:405729694759:web:01770748a583900b282a02",
+  measurementId: "G-WGFKGPB1VL"
   };
 
 firebase.initializeApp(firebaseConfig);
@@ -26,8 +26,14 @@ function addItem(){
 
       createtag="<p>"+item+"</p>"
 
-      document.getElementById("output").innerHTML+= createtagz
+      document.getElementById("output").innerHTML+= createtag
     })
   })
+}
+
+function killItem(){
+  var kill_item = document.getElementById("input").value;
+  firebase.database().ref("/").child(kill_item).remove()
+
 }
   
